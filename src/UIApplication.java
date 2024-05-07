05aaa441c0ce629aa5d851061326ac7d61644d41
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-05-06 21:17:37 -0300

    update UIAplication

--- src/UIApplication.java
+++ src/UIApplication.java
@@ -1,6 +1,4 @@
-import com.studentscrud.factory.ConnectionFactory;
 import com.studentscrud.frames.MainFrame;
-import com.studentscrud.frames.StudentsFrame;
 
 public class UIApplication {
     public static void main(String[] args) {
