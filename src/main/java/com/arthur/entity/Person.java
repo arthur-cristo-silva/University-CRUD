feb59a8c043e2f2342d16a20b9d44b84a2975682
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-05-21 13:08:37 -0300

    feat: new Person attribute, constructors, getters and setters

--- src/main/java/com/arthur/entity/Person.java
+++ src/main/java/com/arthur/entity/Person.java
@@ -3,4 +3,44 @@ package com.arthur.entity;
 public class Person {
     private Long ra;
     private String name;
+    private String type;
+
+    public Person(String type) {
+        this.type = type;
+    }
+
+    public Person(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public Person(Long ra, String name, String type) {
+        this.ra = ra;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getRa() {
+        return ra;
+    }
+
+    public void setRa(Long ra) {
+        this.ra = ra;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
