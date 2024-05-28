4ff623e031c99335df416136552656e9589719b1
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-05-28 16:39:41 -0300

    feat: create UC entity

---
+++ src/main/java/com/arthur/entity/Uc.java
@@ -0,0 +1,40 @@
+package com.arthur.entity;
+
+public class Uc {
+    private String code;
+    private String name;
+    private String type;
+
+    public Uc() {
+    }
+
+    public Uc(String code, String name, String type) {
+        this.code = code;
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
